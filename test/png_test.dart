a495288f081c84336f56fd1ca669bfe6b5949609
Brendan Duncan <brendanduncan@gmail.com>
2014-01-14 15:22:23 -0800

    update png test comment

--- test/png_test.dart
+++ test/png_test.dart
@@ -30,6 +30,33 @@ void definePngTests() {
       if (f is! Io.File || !f.path.endsWith('.png')) {
         return;
       }
+
+      // PngSuite File naming convention:
+      // filename:                                g04i2c08.png
+      //                                          || ||||
+      //  test feature (in this case gamma) ------+| ||||
+      //  parameter of test (here gamma-value) ----+ ||||
+      //  interlaced or non-interlaced --------------+|||
+      //  color-type (numerical) ---------------------+||
+      //  color-type (descriptive) --------------------+|
+      //  bit-depth ------------------------------------+
+      //
+      //  color-type:
+      //
+      //    0g - grayscale
+      //    2c - rgb color
+      //    3p - paletted
+      //    4a - grayscale + alpha channel
+      //    6a - rgb color + alpha channel
+      //    bit-depth:
+      //      01 - with color-type 0, 3
+      //      02 - with color-type 0, 3
+      //      04 - with color-type 0, 3
+      //      08 - with color-type 0, 2, 3, 4, 6
+      //      16 - with color-type 0, 2, 4, 6
+      //      interlacing:
+      //        n - non-interlaced
+      //        i - interlaced
       String name = f.path.split('/').last;
       test('png/$name', () {
         Io.File file = f;
