c9418f21ddd558b7d289b4ed6b5f46d2bbf5aa32
Brendan Duncan <brendanduncan@gmail.com>
2014-01-30 15:16:02 -0800

    Add readWebP high-level function

--- lib/src/formats/formats.dart
+++ lib/src/formats/formats.dart
@@ -1,29 +1,51 @@
 part of image;
 
 
+/**
+ * Decode a JPG formatted image.
+ */
 Image readJpg(List<int> bytes) {
   return new JpegDecoder().decode(bytes);
 }
 
+/**
+ * Encode an image to the JPEG format.
+ */
 List<int> writeJpg(Image image, {int quality: 100}) {
   return new JpegEncoder(quality: quality).encode(image);
 }
 
-
+/**
+ * Decode a PNG formatted image.
+ */
 Image readPng(List<int> bytes) {
   return new PngDecoder().decode(bytes);
 }
 
+/**
+ * Encode an image to the PNG format.
+ */
 List<int> writePng(Image image, {int level: 6}) {
   return new PngEncoder(level: level).encode(image);
 }
 
-
+/**
+ * Decode a Targa formatted image.
+ */
 Image readTga(List<int> bytes) {
   return new TgaDecoder().decode(bytes);
 }
 
+/**
+ * Encode an image to the Targa format.
+ */
 List<int> writeTga(Image image) {
   return new TgaEncoder().encode(image);
 }
 
+/**
+ * Decode a WebP formatted image (first frame for animations).
+ */
+Image readWebP(List<int> bytes) {
+  return new WebPDecoder().decodeImage(bytes);
+}
