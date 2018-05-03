443f6b91c58f8de84bb7226d514840b9fbd0ce09
Keerti Parthasarathy <keertip@google.com>
2018-05-03 14:40:14 -0700

    Fix a Dart 2 runtime failure

--- lib/src/filter/smooth.dart
+++ lib/src/filter/smooth.dart
@@ -9,7 +9,7 @@ part of image;
 Image smooth(Image src, num w) {
   List<double> filter = [
     1.0, 1.0, 1.0,
-    1.0, w,   1.0,
+    1.0, w.toDouble(),   1.0,
     1.0, 1.0, 1.0];
 
   return convolution(src, filter, w + 8, 0);
