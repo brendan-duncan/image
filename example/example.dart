2ee0a426a2113af130ef0528e76af7514f61de6e
Brendan Duncan <brendanduncan@gmail.com>
2018-06-06 21:04:21 -0700

    Add an example for loading and processing an image in a separate isolate thread.

--- example/example.dart
+++ example/example.dart
@@ -1,15 +1,14 @@
-import 'dart:io' as Io;
+import 'dart:io';
 import 'package:image/image.dart';
 void main() {
   // Read an image from file (webp in this case).
   // decodeImage will identify the format of the image and use the appropriate
   // decoder.
-  Image image = decodeImage(new Io.File('test.webp').readAsBytesSync());
+  Image image = decodeImage(new File('test.webp').readAsBytesSync());
 
   // Resize the image to a 120x? thumbnail (maintaining the aspect ratio).
   Image thumbnail = copyResize(image, 120);
 
   // Save the thumbnail as a PNG.
-  new Io.File('thumbnail.png')
-        ..writeAsBytesSync(encodePng(thumbnail));
+  new File('thumbnail.png').writeAsBytesSync(encodePng(thumbnail));
 }
---
+++ example/image_isolate.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'dart:isolate';
+import 'package:image/image.dart';
+
+class DecodeParam {
+  final File file;
+  final SendPort sendPort;
+  DecodeParam(this.file, this.sendPort);
+}
+
+void decode(DecodeParam param) {
+  // Read an image from file (webp in this case).
+  // decodeImage will identify the format of the image and use the appropriate
+  // decoder.
+  Image image = decodeImage(param.file.readAsBytesSync());
+  // Resize the image to a 120x? thumbnail (maintaining the aspect ratio).
+  Image thumbnail = gaussianBlur(copyResize(image, 120), 5);
+  param.sendPort.send(thumbnail);
+}
+
+// Decode and process an image file in a separate thread (isolate) to avoid
+// stalling the main UI thread.
+void main() async {
+  ReceivePort receivePort = new ReceivePort();
+
+  await Isolate.spawn(decode,
+      new DecodeParam(new File('test.webp'), receivePort.sendPort));
+
+  // Get the processed image from the isolate.
+  Image image = await receivePort.first;
+
+  new File('thumbnail.png').writeAsBytesSync(encodePng(image));
+}
