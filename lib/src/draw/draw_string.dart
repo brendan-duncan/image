bf53e9a4d126644d6f65b8cae32767d204094c0d
Brendan Duncan <brendanduncan@gmail.com>
2018-12-01 21:57:27 -0800

    optimize drawString

--- lib/src/draw/draw_string.dart
+++ lib/src/draw/draw_string.dart
@@ -1,7 +1,13 @@
+import 'dart:typed_data';
 import '../bitmap_font.dart';
 import '../color.dart';
 import '../image.dart';
 import 'draw_pixel.dart';
+
+var _r_lut = new Uint8List(256);
+var _g_lut = new Uint8List(256);
+var _b_lut = new Uint8List(256);
+var _a_lut = new Uint8List(256);
 
 /**
  * Draw a string horizontally into [image] horizontally into [image] at position
@@ -11,16 +17,23 @@ import 'draw_pixel.dart';
  * such as: [arial_14], [arial_24], or [arial_48].
  */
 Image drawString(Image image, BitmapFont font, int x, int y, String string,
-                 {int color: 0xffffffff}) {
-  int ca = getAlpha(color);
-  if (ca == 0) {
-    return image;
+                 {int color = 0xffffffff}) {
+  if (color != 0xffffffff) {
+    int ca = getAlpha(color);
+    if (ca == 0) {
+      return image;
+    }
+    double da = ca / 255.0;
+    double dr = getRed(color) / 255.0;
+    double dg = getGreen(color) / 255.0;
+    double db = getBlue(color) / 255.0;
+    for (int i = 1; i < 256; ++i) {
+      _r_lut[i] = (dr * i).toInt();
+      _g_lut[i] = (dg * i).toInt();
+      _b_lut[i] = (db * i).toInt();
+      _a_lut[i] = (da * i).toInt();
+    }
   }
-
-  double da = ca / 255.0;
-  double dr = getRed(color) / 255.0;
-  double dg = getGreen(color) / 255.0;
-  double db = getBlue(color) / 255.0;
 
   List<int> chars = string.codeUnits;
   for (int c in chars) {
@@ -37,11 +50,12 @@ Image drawString(Image image, BitmapFont font, int x, int y, String string,
     for (int yi = y; yi < y2; ++yi) {
       for (int xi = x; xi < x2; ++xi) {
         int p = ch.image[pi++];
-        p = getColor((getRed(p) * dr).toInt(),
-                     (getGreen(p) * dg).toInt(),
-                     (getBlue(p) * db).toInt(),
-                     (getAlpha(p) * da).toInt());
-
+        if (color != 0xffffffff) {
+          p = getColor(_r_lut[getRed(p)],
+              _g_lut[getGreen(p)],
+              _b_lut[getBlue(p)],
+              _a_lut[getAlpha(p)]);
+        }
         drawPixel(image, xi + ch.xoffset, yi + ch.yoffset, p);
       }
     }
