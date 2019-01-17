d76806b3d4af2d103d322c7bfb2b57bf354ac0e7
Brendan Duncan <brendanduncan@gmail.com>
2019-01-16 21:46:34 -0800

    roll back xml changes

--- lib/src/bitmap_font.dart
+++ lib/src/bitmap_font.dart
@@ -300,17 +300,17 @@ class BitmapFont {
       switch (tk[0]) {
         case 'info':
           var attrs = _parseParameters(tk);
-          var info = new XML.XmlElement(new XML.XmlName('info'), attributes: attrs, children: []);
+          var info = new XML.XmlElement(new XML.XmlName('info'), attrs, []);
           children.add(info);
           break;
         case 'common':
           var attrs = _parseParameters(tk);
-          var node = new XML.XmlElement(new XML.XmlName('common'), attributes: attrs, children: []);
+          var node = new XML.XmlElement(new XML.XmlName('common'), attrs, []);
           children.add(node);
           break;
         case 'page':
           var attrs = _parseParameters(tk);
-          var page = new XML.XmlElement(new XML.XmlName('page'), attributes: attrs, children: []);
+          var page = new XML.XmlElement(new XML.XmlName('page'), attrs, []);
           pageList.add(page);
           break;
         case 'chars':
@@ -318,7 +318,7 @@ class BitmapFont {
           break;
         case 'char':
           var attrs = _parseParameters(tk);
-          var node = new XML.XmlElement(new XML.XmlName('char'), attributes: attrs, children: []);
+          var node = new XML.XmlElement(new XML.XmlName('char'), attrs, []);
           charList.add(node);
           break;
         case 'kernings':
@@ -326,30 +326,30 @@ class BitmapFont {
           break;
         case 'kerning':
           var attrs = _parseParameters(tk);
-          var node = new XML.XmlElement(new XML.XmlName('kerning'), attributes: attrs, children: []);
+          var node = new XML.XmlElement(new XML.XmlName('kerning'), attrs, []);
           kerningList.add(node);
           break;
       }
     }
 
     if (charsAttrs != null || charList.isNotEmpty) {
-      var node = new XML.XmlElement(new XML.XmlName('chars'), attributes: charsAttrs,
-          children: charList);
+      var node = new XML.XmlElement(new XML.XmlName('chars'), charsAttrs,
+          charList);
       children.add(node);
     }
 
     if (kerningsAttrs != null || kerningList.isNotEmpty) {
-      var node = new XML.XmlElement(new XML.XmlName('kernings'), attributes: kerningsAttrs,
-          children: kerningList);
+      var node = new XML.XmlElement(new XML.XmlName('kernings'), kerningsAttrs,
+          kerningList);
       children.add(node);
     }
 
     if (pageList.isNotEmpty) {
-      var pages = new XML.XmlElement(new XML.XmlName('pages'), attributes: [], children: pageList);
+      var pages = new XML.XmlElement(new XML.XmlName('pages'), [], pageList);
       children.add(pages);
     }
 
-    var xml = new XML.XmlElement(new XML.XmlName('font'), attributes: [], children: children);
+    var xml = new XML.XmlElement(new XML.XmlName('font'), [], children);
     var doc = new XML.XmlDocument([xml]);
 
     return doc;
