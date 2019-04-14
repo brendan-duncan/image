27cd4e6280b20abecc5bd44439f7a2e82e12f304
Kyaw PC <kyawtun@yathit.com>
2019-04-14 11:07:23 +0800

    add rectify method

---
+++ lib/src/transform/transform.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+import '../image.dart';
+
+/**
+ * Transform image [src] to rectify perspective with oblique quadrilateral
+ * by given four corners [topLeft], [topRight], [bottomLeft], [bottomRight].
+ */
+Image rectify(Image src, Point topLeft, Point topRight, Point bottomLeft,
+    Point bottomRight) {
+  Image dstImage;
+  for (int y = 0; y < dstImage.height; ++y) {
+    double v = y / (dstImage.height - 1);
+    for (int x = 0; x < dstImage.width; ++x) {
+      double u = x / (dstImage.width - 1);
+      // bilinear interpolation
+      Point srcPixelCoord = topLeft * (1 - u) * (1 - v) +
+          topRight * (u) * (1 - v) +
+          bottomLeft * (1 - u) * (v) +
+          bottomRight * (u) * (v);
+      var srcPixel = src.getPixel(srcPixelCoord.x, srcPixelCoord.y);
+      dstImage.setPixel(x, y, srcPixel);
+    }
+  }
+  return dstImage;
+}
