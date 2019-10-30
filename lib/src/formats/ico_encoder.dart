a8b3438aab36a625efb37b39e248efb8b5ceae70
SimonIT <simonit.orig@gmail.com>
2019-10-30 09:34:03 +0100

    throw exception if image to large, cur implementation with abstract class

--- lib/src/formats/ico_encoder.dart
+++ lib/src/formats/ico_encoder.dart
@@ -4,7 +4,13 @@ import '../image.dart';
 import '../util/output_buffer.dart';
 import 'encoder.dart';
 
-class IcoEncoder extends Encoder {
+abstract class WinEncoder extends Encoder {
+  int get type;
+
+  int colorPlanesOrXHotSpot(int index);
+
+  int bitsPerPixelOrYHotSpot(int index);
+
   @override
   List<int> encodeImage(Image image) {
     return encodeImages([image]);
@@ -17,20 +23,23 @@ class IcoEncoder extends Encoder {
 
     // header
     out.writeUint16(0); // reserved
-    out.writeUint16(1); // type: ICO => 1; CUR => 2
+    out.writeUint16(type); // type: ICO => 1; CUR => 2
     out.writeUint16(count);
 
     int offset = 6 + count * 16; // file header with image directory byte size
 
     List<List<int>> imageDatas = [];
 
+    int i = 0;
     for (Image img in images) {
+      if (img.width > 256 || img.height > 256) throw Exception("ICO and CUR support only sizes until 256");
+
       out.writeByte(img.width); // image width in pixels
       out.writeByte(img.height); // image height in pixels
       out.writeByte(0); // Color count, should be 0 if more than 256 colors https://fileformats.fandom.com/wiki/Icon
       out.writeByte(0); // Reserved
-      out.writeUint16(0); // Color planes
-      out.writeUint16(32); // bits per pixel
+      out.writeUint16(colorPlanesOrXHotSpot(i));
+      out.writeUint16(bitsPerPixelOrYHotSpot(i));
 
       List<int> data =
           PngEncoder().encodeImage(img); // Use png instead of bmp encoded data, it's supported since Windows Vista
@@ -39,6 +48,7 @@ class IcoEncoder extends Encoder {
       out.writeUint32(offset); // offset of data from the beginning of the file
 
       offset += data.length; // add the size of bytes to get the new begin of the next image
+      i++;
       imageDatas.add(data);
     }
 
@@ -48,4 +58,15 @@ class IcoEncoder extends Encoder {
 
     return out.getBytes();
   }
+}
+
+class IcoEncoder extends WinEncoder {
+  @override
+  int colorPlanesOrXHotSpot(int index) => 0;
+
+  @override
+  int bitsPerPixelOrYHotSpot(int index) => 32;
+
+  @override
+  int get type => 1;
 }
