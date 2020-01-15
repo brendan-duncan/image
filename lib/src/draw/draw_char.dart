1053c37dbd4b46b387e79a4a0802837d6f50221d
brendan_duncan <brendanduncan@gmail.com>
2020-01-14 22:03:47 -0800

    drawChar didn't respect color parameter

--- lib/src/draw/draw_char.dart
+++ lib/src/draw/draw_char.dart
@@ -5,7 +5,7 @@ import 'draw_pixel.dart';
 /// Draw a single character from [char] horizontally into [image] at position
 /// [x],[y] with the given [color].
 Image drawChar(Image image, BitmapFont font, int x, int y, String char,
-    {int color = 0xffffffff}) {
+    {int color}) {
   var c = char.codeUnits[0];
   if (!font.characters.containsKey(c)) {
     return image;
@@ -17,8 +17,11 @@ Image drawChar(Image image, BitmapFont font, int x, int y, String char,
   var pi = 0;
   for (var yi = y; yi < y2; ++yi) {
     for (var xi = x; xi < x2; ++xi) {
-      var p = ch.image[pi++];
-      drawPixel(image, xi, yi, p);
+      if (color != null) {
+        drawPixel(image, xi, yi, color);
+      } else {
+        drawPixel(image, xi, yi, ch.image[pi++]);
+      }
     }
   }
 
