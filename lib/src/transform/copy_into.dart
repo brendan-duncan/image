43d67c6a24f8ae3f36e106022d1772cfe12b3bb4
Adnan <adnanfahedhr@hotmail.com>
2021-07-05 14:24:36 +0300

    copyInto: comments and center parameter

--- lib/src/transform/copy_into.dart
+++ lib/src/transform/copy_into.dart
@@ -14,20 +14,44 @@ import '../image.dart';
 /// The coordinates refer to the upper left corner. This function can be used to
 /// copy regions within the same image (if [dst] is the same as [src])
 /// but if the regions overlap the results will be unpredictable.
-Image copyInto(Image dst, Image src,
-    {int? dstX,
-    int? dstY,
-    int? srcX,
-    int? srcY,
-    int? srcW,
-    int? srcH,
-    bool blend = true}) {
+///
+/// [dstX] and [dstY] represent the X and Y position where the [src] will start
+/// printing.
+///
+/// if [center] is true, the [src] will be centered in [dst].
+Image copyInto(
+  Image dst,
+  Image src, {
+  int? dstX,
+  int? dstY,
+  int? srcX,
+  int? srcY,
+  int? srcW,
+  int? srcH,
+  bool blend = true,
+  bool center = false,
+}) {
   dstX ??= 0;
   dstY ??= 0;
   srcX ??= 0;
   srcY ??= 0;
   srcW ??= src.width;
   srcH ??= src.height;
+
+  if (center) {
+    {
+      // if [src] is wider than [dst]
+      var wdt = (dst.width - src.width);
+      if (wdt < 0) wdt = 0;
+      dstX = wdt ~/ 2;
+    }
+    {
+      // if [src] is higher than [dst]
+      var hight = (dst.height - src.height);
+      if (hight < 0) hight = 0;
+      dstY = hight ~/ 2;
+    }
+  }
 
   if (blend) {
     for (var y = 0; y < srcH; ++y) {
