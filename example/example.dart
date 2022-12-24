c05716e8c49113d92117154fc2c68049f10fb006
Brendan Duncan <brendanduncan@gmail.com>
2022-12-23 16:48:46 -0800

    Update example.dart

--- example/example.dart
+++ example/example.dart
@@ -3,7 +3,7 @@ import 'package:image/image.dart' as DIL;
 void main() async {
   final cmd = DIL.Command()
     // Read a WebP image from a file.
-    ..decodeImageFile('test.webp')
+    ..decodeWebPFile('test.webp')
     // Resize the image so its width is 120 and height maintains aspect ratio.
     ..copyResize(width: 120)
     // Save the image to a PNG file.
