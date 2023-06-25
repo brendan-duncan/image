d4cf77ef0101a66ffd7830f35de71425c8ea3e64
Brendan Duncan <brendanduncan@gmail.com>
2023-06-25 09:50:26 -0700

    Update copy_resize test

--- test/transform/copy_resize_test.dart
+++ test/transform/copy_resize_test.dart
@@ -11,7 +11,7 @@ void main() {
           decodePng(File('test/_data/png/buck_24.png').readAsBytesSync())!;
       final i0 = copyResize(img, width: 64);
       expect(i0.width, equals(64));
-      expect(i0.height, equals(39));
+      expect(i0.height, equals(40));
       File('$testOutputPath/transform/copyResize.png')
         ..createSync(recursive: true)
         ..writeAsBytesSync(encodePng(i0));
