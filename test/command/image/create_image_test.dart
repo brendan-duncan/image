17b0420566f7020629944ce8c87bec66cc280676
Brendan Duncan <brendanduncan@gmail.com>
2024-01-24 11:16:23 -0700

    Add test for using Future with Command.executeThread

--- test/command/image/create_image_test.dart
+++ test/command/image/create_image_test.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:image/image.dart';
 import 'package:test/test.dart';
@@ -35,6 +36,35 @@ void main() {
           File('$testOutputPath/cmd/createImage_1.png').existsSync(), isTrue);
       expect(
           File('$testOutputPath/cmd/createImage_2.png').existsSync(), isTrue);
+    });
+
+    test('Futures', () async {
+      final image = Image(width: 256, height: 256);
+
+      final List<Future<Uint8List>> resTask = [];
+      const curW = 128;
+      const curH = 128;
+      for (var curY = 0; curY < image.height; curY += curH) {
+        for (var curX = 0; curX < image.width; curX += curW) {
+          final task = Future(() async {
+            final cmd = Command()
+              ..image(image) // This will copy the image to the isolate
+              ..copyCrop(x: curX, y: curY, width: curW, height: curH)
+              ..encodeJpg();
+            // Execute the commands in an Isolate thread and wait for the
+            // results.
+            final result = await cmd.executeThread();
+            // The resulting bytes of the last command
+            return result.outputBytes!;
+          });
+          resTask.add(task);
+        }
+      }
+
+      await Future.wait(resTask).then((value) {
+        // 4 jpeg files with the cropped images
+        expect(value.length, equals(4));
+      });
     });
   });
 }
