615c4da21480b5b66c315beda3bec2fd693a518a
Brendan Duncan <brendanduncan@gmail.com>
2024-07-07 10:37:25 -0600

    Add palette image to trim test

--- test/transform/trim_test.dart
+++ test/transform/trim_test.dart
@@ -8,27 +8,27 @@ void main() {
   group('Transform', () {
     test('trim', () {
       final image =
-          decodePng(File('test/_data/png/trim.png').readAsBytesSync())!;
+          decodePng(File('test/_data/png/logo.png').readAsBytesSync())!;
       var trimmed = trim(image);
       File('$testOutputPath/transform/trim.png')
         ..createSync(recursive: true)
         ..writeAsBytesSync(encodePng(trimmed));
-      expect(trimmed.width, equals(64));
-      expect(trimmed.height, equals(56));
+      expect(trimmed.width, equals(465));
+      expect(trimmed.height, equals(150));
 
       trimmed = trim(image, mode: TrimMode.transparent);
       File('$testOutputPath/transform/trim_transparent.png')
         ..createSync(recursive: true)
         ..writeAsBytesSync(encodePng(trimmed));
-      expect(trimmed.width, equals(image.width));
+      expect(trimmed.width, 465);
       expect(trimmed.height, equals(image.height));
 
       trimmed = trim(image, mode: TrimMode.bottomRightColor);
       File('$testOutputPath/transform/trim_bottomRightColor.png')
         ..createSync(recursive: true)
         ..writeAsBytesSync(encodePng(trimmed));
-      expect(trimmed.width, equals(64));
-      expect(trimmed.height, equals(56));
+      expect(trimmed.width, 465);
+      expect(trimmed.height, 150);
     });
   });
 }
